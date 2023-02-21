d6fdcff53f50ad3da2710a39bbf4a81b6e7f23fd
Lais Souza <107737807+LaisSouza1@users.noreply.github.com>
2023-02-21 01:54:24 -0300

    Add files via upload

---
+++ ex014.js
@@ -0,0 +1,6 @@
+const custoProduto = 25
+const precoVenda = 50
+
+let lucro = (precoVenda - custoProduto) * 1000
+
+console.log(`A empresa terá o lucro de R$ ${lucro} na venda de mil exemplares desse produto`)
