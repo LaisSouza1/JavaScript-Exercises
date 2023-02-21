051d76858d497a57064125540f22078f9bcf911c
Lais Souza <107737807+LaisSouza1@users.noreply.github.com>
2023-02-21 01:57:32 -0300

    Delete ex014.js

--- ex014.js
+++
@@ -1,6 +0,0 @@
-const custoProduto = 25
-const precoVenda = 50
-
-let lucro = (precoVenda - custoProduto) * 1000
-
-console.log(`A empresa terá o lucro de R$ ${lucro} na venda de mil exemplares desse produto`)
